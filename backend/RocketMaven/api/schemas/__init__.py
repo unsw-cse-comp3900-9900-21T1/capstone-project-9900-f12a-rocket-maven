22e172a8798854a49d0b1dc3bb00b0f1bbfab5b3
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-03-02 04:37:46 +1100

    Added back end

---
+++ backend/RocketMaven/api/schemas/__init__.py
@@ -0,0 +1,4 @@
+from RocketMaven.api.schemas.user import UserSchema
+
+
+__all__ = ["UserSchema"]
---
+++ backend/RocketMaven/manage.py
@@ -0,0 +1,25 @@
+import click
+from flask.cli import with_appcontext
+
+
+@click.group()
+def cli():
+    """Main entry point"""
+
+
+@cli.command("init")
+@with_appcontext
+def init():
+    """Create a new admin user"""
+    from RocketMaven.extensions import db
+    from RocketMaven.models import User
+
+    click.echo("create user")
+    user = User(username="admin", email="admin@mail.com", password="admin", active=True)
+    db.session.add(user)
+    db.session.commit()
+    click.echo("created user admin")
+
+
+if __name__ == "__main__":
+    cli()
