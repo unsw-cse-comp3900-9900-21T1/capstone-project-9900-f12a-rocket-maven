af3a22b5e8fb614193da5cf31ac250802a562ff6
Tyson Tong <tyson.tong90@gmail.com>
2021-03-04 16:35:39 +1100

    fixed url definitions, created basic detail components

--- client/src/App/Routes/index.tsx
+++ client/src/App/Routes/index.tsx
@@ -1,14 +1,18 @@
+import { url } from 'inspector'
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import HomeStub from "../../pages/HomeStub"
 import LogIn from "../../pages/LogIn"
+import PortDetails from "../../pages/PortDetails"
+import { urls } from '../../pages/urls'
 
 const Routes = () => (
   <Switch>
     {/* TODO(Jude): Distinguish between logged in and logged out routes */}
     {/* TODO(Jude): Make root path a redirect depending on logged in status*/}
-    <Route exact path="/" component={HomeStub} />
-    <Route exact path="/login-stub" component={LogIn} />
+    <Route exact path={urls.root} component={HomeStub} />
+    <Route exact path={urls.loginStub} component={LogIn} />
+    <Route exact path={urls.portfolioDetails} component={PortDetails}/>
   </Switch>
 )
 
---
+++ client/src/pages/PortDetails/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Page from '../_Page'
+import { StubWrap } from '../../componentsStyled/Layouts'
+import { Title } from '../../componentsStyled/Typography'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import { MyCheckbox, MySelect, MyTextInput } from '../../forms'
+ import * as Yup from 'yup'
+
+// TODO(Jude): convert to typescript
+const PortDetails = () => {
+
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default PortDetails
