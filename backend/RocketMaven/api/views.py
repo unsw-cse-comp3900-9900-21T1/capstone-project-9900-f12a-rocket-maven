b8684f7816df09f2de77c2a3af5bb60812a35c0e
judemlim <judemlim@gmail.com>
2021-03-07 12:58:33 +1100

    Setup - Implementation of temporary stub endpoints in backend for front-end testing

--- backend/RocketMaven/api/views.py
+++ backend/RocketMaven/api/views.py
@@ -2,12 +2,16 @@ from flask import Blueprint, current_app, jsonify
 from flask_restful import Api
 from marshmallow import ValidationError
 from RocketMaven.extensions import apispec
-from RocketMaven.api.resources import UserResource, UserList
+from RocketMaven.api.resources import UserResource, UserList, Time, LoginStub
 from RocketMaven.api.schemas import UserSchema
 
 
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(blueprint)
+
+# Stub endpoints for front end testing
+api.add_resource(Time, "/get-time", endpoint="current_time")
+api.add_resource(LoginStub, "/login-stub", endpoint="login_stub")
 
 
 api.add_resource(UserResource, "/users/<int:user_id>", endpoint="user_by_id")
@@ -19,6 +23,10 @@ def register_views():
     apispec.spec.components.schema("UserSchema", schema=UserSchema)
     apispec.spec.path(view=UserResource, app=current_app)
     apispec.spec.path(view=UserList, app=current_app)
+
+    # Stub endpoints for pront end testing 
+    apispec.spec.path(view=Time, app=current_app)
+    apispec.spec.path(view=LoginStub, app=current_app)
 
 
 @blueprint.errorhandler(ValidationError)
