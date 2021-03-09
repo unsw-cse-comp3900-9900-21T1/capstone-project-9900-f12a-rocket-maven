9b7ac50d4b3287d69245cc8adf05cc88fa528b99
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-03-09 20:13:39 +1100

    Fix development environment temp admin recreation

--- backend/RocketMaven/manage.py
+++ backend/RocketMaven/manage.py
@@ -17,15 +17,18 @@ def init():
     db.create_all()
 
     click.echo("create user")
-    user = Investor(
-        username="temp_admin",
-        email="admin@mail.com",
-        password="WjjTeWGdylJkH2Pq",
-        email_verified=True,
-        country_of_residency="AU",
-    )
-    db.session.add(user)
-    db.session.commit()
+    test_user = db.session.query(Investor).filter_by(username="temp_admin").first()
+
+    if not test_user:
+        user = Investor(
+            username="temp_admin",
+            email="admin@mail.com",
+            password="WjjTeWGdylJkH2Pq",
+            email_verified=True,
+            country_of_residency="AU",
+        )
+        db.session.add(user)
+        db.session.commit()
     click.echo("created user admin")
 
 
