ee8ac69992ca275f01824aa35cb2fba78696648b
SydP <syd.pleno@gmail.com>
2021-03-13 23:48:40 +1100

    Remove event_date from request parameter for POST /api/v1/portfolios/{portfolio_id}

--- backend/RocketMaven/api/schemas/portfolio_event.py
+++ backend/RocketMaven/api/schemas/portfolio_event.py
@@ -5,11 +5,10 @@ from RocketMaven.extensions import ma, db
 class PortfolioEventSchema(ma.SQLAlchemyAutoSchema):
 
     id = ma.Int(dump_only=True)
-    # TODO date_created = ma.Int(dump_only=True)
-    # password = ma.String(load_only=True, required=True)
-
+    event_date = ma.DateTime(dump_only=True)
+    
     class Meta:
         model = PortfolioEvent
         sqla_session = db.session
         load_instance = True
-        # exclude = ("id",)
+    
