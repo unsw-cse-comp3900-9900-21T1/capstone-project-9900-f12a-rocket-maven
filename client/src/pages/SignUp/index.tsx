be1571d58ae5e3499167782b0631d81c52f0c4d2
Tyson Tong <tyson.tong90@gmail.com>
2021-03-13 21:49:56 +1100

    build the basic sign up components

--- client/src/pages/SignUp/index.tsx
+++ client/src/pages/SignUp/index.tsx
@@ -1,104 +1,138 @@
-import { useContext } from 'react'
-import Page from '../_Page'
-import { Title } from '../../componentsStyled/Typography'
-import { Formik, Field, Form, ErrorMessage } from 'formik'
-import { MyCheckbox, MySelect, MyTextInput } from '../../forms'
-import { storeContext } from '../../data/store'
- import * as Yup from 'yup'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Form, Input, Button, Checkbox } from 'antd';
 
-const LogIn = () => {
-  const { dispatch } = useContext(storeContext)
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const SignUp = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+
+  };
 
   return (
-    <Page>
-      <Title>
-        Login Stub
-      </Title>
-      {/* TODO(Jude): Refactor and minimise: Seprate schema */}
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-          acceptedTerms: false, // added for our checkbox
-          jobType: '', // added for our select
-        }}
-        validationSchema={Yup.object({
-          firstName: Yup.string()
-            .max(15, 'Must be 15 characters or less')
-            .required('Required'),
-          lastName: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required('Required'),
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('Required'),
-          acceptedTerms: Yup.boolean()
-            .required('Required')
-            .oneOf([true], 'You must accept the terms and conditions.'),
-          jobType: Yup.string()
-            .oneOf(
-              ['designer', 'development', 'product', 'other'],
-              'Invalid Job Type'
-            )
-            .required('Required'),
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          fetch('/api/v1/login-stub', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(values)
-          })
-          .then(res => res.json())
-          .then(data => {
-            console.log("************** response is", data)
-            if (data.msg === 'Form submission successfull') {
-              dispatch({type: "LOGIN"})
-            }
-          })
-          // TODO(Jude): Maybe add error and success snack bars here
-        }}
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
       >
-        <Form>
-          <MyTextInput
-            label="First Name"
-            name="firstName"
-            type="text"
-            placeholder="Jane"
-          />
+        <Input />
+      </Form.Item>
 
-          <MyTextInput
-            label="Last Name"
-            name="lastName"
-            type="text"
-            placeholder="Doe"
-          />
+      <Form.Item
+        label="First Name"
+        name="firstName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your first name!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-          <MyTextInput
-            label="Email Address"
-            name="email"
-            type="email"
-            placeholder="jane@formik.com"
-          />
+      <Form.Item
+        label="Last Name"
+        name="lastName"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your last name!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-          <MySelect label="Job Type" name="jobType">
-            <option value="">Select a job type</option>
-            <option value="designer">Designer</option>
-            <option value="development">Developer</option>
-            <option value="product">Product Manager</option>
-            <option value="other">Other</option>
-          </MySelect>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-          <MyCheckbox name="acceptedTerms">
-            I accept the terms and conditions
-          </MyCheckbox>
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </Page>
-  )
-}
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-export default LogIn
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject(new Error('The two passwords that you entered do not match!'));
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default SignUp
