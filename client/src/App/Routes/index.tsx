ff370544fd1dae5c22875345fb3d362a8194a0ea
Tyson Tong <tyson.tong90@gmail.com>
2021-03-13 20:31:59 +1100

    create a signup skeleton

--- client/src/App/Routes/index.tsx
+++ client/src/App/Routes/index.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route, Redirect } from 'react-router-dom'
 import HomeStub from "../../pages/HomeStub"
 import LogIn from "../../pages/LogIn"
+import SignUp from "../../pages/SignUp"
 import PortfolioDetail from "../../pages/PortfolioDetail"
 import { urls } from '../../pages/urls'
 
@@ -10,6 +11,7 @@ const Routes = () => (
     {/* TODO(Jude): Make root path a redirect depending on logged in status*/}
     <Route exact path={urls.root} component={HomeStub} />
     <Route exact path={urls.login} component={LogIn} />
+    <Route exact path={urls.signup} component={SignUp} />
     <Route exact path={urls.portfolioDetails} component={PortfolioDetail}/>
   </Switch>
 )
--- client/src/pages/urls.ts
+++ client/src/pages/urls.ts
@@ -5,5 +5,6 @@ export const urls = {
   /** Root URL which would then redirect to the appropriate page */
   root: "/",
   login: '/login',
+  signup: '/signup',
   portfolioDetails: '/portfolio-details'
 }
