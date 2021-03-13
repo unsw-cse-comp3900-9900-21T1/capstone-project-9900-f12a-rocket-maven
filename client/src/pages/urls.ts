908d3c83386e47572eedd5777a778d31f3bd729a
Tyson Tong <tyson.tong90@gmail.com>
2021-03-14 01:46:37 +1100

    build log in 2 skeleton, using Ant Design

--- client/src/pages/urls.ts
+++ client/src/pages/urls.ts
@@ -5,6 +5,7 @@ export const urls = {
   /** Root URL which would then redirect to the appropriate page */
   root: "/",
   login: '/login',
+  login2: '/login2', //Tyson's LogIn page
   signup: '/signup',
   portfolioDetails: '/portfolio-details'
 }
