97bd966e8e3faf5b35e8adf25b35b6c662361638
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-03-14 12:26:39 +1100

    fix(backend): complete correct portfolio asset holding flow

--- backend/RocketMaven/api/schemas/portfolio_asset_holding.py
+++ backend/RocketMaven/api/schemas/portfolio_asset_holding.py
@@ -3,9 +3,13 @@ from RocketMaven.extensions import ma, db
 
 
 class PortfolioAssetHoldingSchema(ma.SQLAlchemyAutoSchema):
-
     class Meta:
         model = PortfolioAssetHolding
         sqla_session = db.session
         load_instance = True
-    
+
+    _unrealised_units = ma.auto_field(
+        data_key="unrealised_units", attribute="unrealised_units"
+    )
+    asset_id = ma.auto_field()
+    portfolio_id = ma.auto_field()
