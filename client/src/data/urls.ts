27212fede782251b3fddd4f6689aa5d4212e9f30
judemlim <judemlim@gmail.com>
2021-03-15 22:36:09 +1100

    Added landing page based on loggedIn status

--- client/src/data/urls.ts
+++ client/src/data/urls.ts
@@ -1,8 +1,7 @@
 export const urls = {
   /** Root URL which would then redirect to the appropriate page */
-  root: "/",
+  root: '/',
   login: '/login',
-  login2: '/login2', //Tyson's LogIn page
   signup: '/signup',
   portfolioDetails: '/portfolio-details',
   // TODO(Jude): Crudify routes 
