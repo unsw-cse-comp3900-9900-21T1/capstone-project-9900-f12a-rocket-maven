f7067327ca519d54c420f4b6b82c34c6a46404d3
SydP <syd.pleno@gmail.com>
2021-03-17 18:18:19 +1100

    Change id and price_last_updated fields to "dump_only" in Asset schema

--- backend/RocketMaven/api/schemas/asset.py
+++ backend/RocketMaven/api/schemas/asset.py
@@ -4,6 +4,8 @@ from RocketMaven.extensions import ma, db
 
 class AssetSchema(ma.SQLAlchemyAutoSchema):
 
+    id = ma.Int(dump_only=True)
+    price_last_updated = ma.Date(dump_only=True)
     class Meta:
         model = Asset
         sqla_session = db.session
