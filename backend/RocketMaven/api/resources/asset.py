400d0d92e25277324a6c514c59da4c04819f7886
SydP <syd.pleno@gmail.com>
2021-03-18 17:43:04 +1100

    chore(backend): Asset/AssetService - Tidy up docstrings and REST response codes

--- backend/RocketMaven/api/resources/asset.py
+++ backend/RocketMaven/api/resources/asset.py
@@ -9,7 +9,6 @@ from RocketMaven.commons.pagination import paginate
 
 
 class AssetResource(Resource):
-
     def get(self, ticker_symbol):
         """
         ---
@@ -30,28 +29,31 @@ class AssetResource(Resource):
                   type: object
                   properties:
                     asset: AssetSchema
+          400:
+            description: Missing ticker symbol
           404:
-            description: asset does not exist
+            description: Asset does not exist
         """
         return AssetService.get_asset(ticker_symbol)
 
 
 class AssetSearchResource(Resource):
-
     def get(self):
         """
         ---
         summary: Search assets
-        description: Search assets
+        description: Search assets based on ticker or company name
         tags:
           - Asset
         parameters:
           - in: query
             name: q
+            description: Parameter to search ticker/company name on
             schema:
               type: string
           - in: query
             name: per_page
+            description: Number of results to return per page
             schema:
               type: string
             default: 10
@@ -63,7 +65,9 @@ class AssetSearchResource(Resource):
                   type: object
                   properties:
                     asset: AssetSchema
+          400:
+            description: Missing search query
           404:
-            description: no assets found
+            description: No assets found
         """
         return AssetService.search_asset()
