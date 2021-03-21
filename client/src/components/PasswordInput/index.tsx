5c7b1cf943eee4ef6fc58c4cc5b821e71fe37684
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-03-22 00:17:17 +1100

    feat(frontend): add consistent password validation
    Added a new PasswordInput React component to handle the password validation criteria consistently. Adapts the back-end modifications from @sydp

---
+++ client/src/components/PasswordInput/index.tsx
@@ -0,0 +1,48 @@
+import { Form, Input } from 'antd'
+
+function ValidationError(message: string) {
+  return Promise.reject(new Error(message))
+}
+
+const special_chars = '~`!@#$%^&*()_+-=[]\\{}|:";\'<>?,./'.split('')
+
+const PasswordInput = () => {
+  return (
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input a password!'
+        },
+        () => ({
+          validator(_, value) {
+            if (!value) return ValidationError('Cannot have an empty password')
+
+            if (value.length < 8)
+              return ValidationError('Password cannot be less than 8 characters')
+
+            if (!/[0-9]/.test(value))
+              return ValidationError('Password must contain at least one digit')
+
+            if (!/[A-Z]/.test(value))
+              return ValidationError('Password must contain at least one capital letter')
+
+            if (!/[a-z]/.test(value))
+              return ValidationError('Password must contain at least one lowercase letter')
+
+            if (!value.split('').some((ai: string) => special_chars.includes(ai)))
+              return ValidationError('Password must contain at least one special character')
+
+            return Promise.resolve()
+          }
+        })
+      ]}
+    >
+      <Input.Password />
+    </Form.Item>
+  )
+}
+
+export default PasswordInput
--- client/src/pages/LogIn/index.tsx
+++ client/src/pages/LogIn/index.tsx
@@ -1,78 +1,84 @@
 import Page from '../_Page'
-import { Fragment  } from 'react'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Link } from 'react-router-dom';
-import 'antd/dist/antd.css';
+import { Fragment } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Link } from 'react-router-dom'
+import 'antd/dist/antd.css'
 
 import { Card } from '../../componentsStyled/Card'
-import { Form, Input, Button} from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Input, Button } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import { urls } from '../../data/urls'
 import { useAuth } from '../../hooks/http'
 
 const LogIn = () => {
-
   const setValuesAndFetch = useAuth('LOGIN')
   const onFinish = (values: any) => {
     setValuesAndFetch(values)
-  };
+  }
 
   return (
     <Page>
-    <Card>  
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Username!',
-            },
-          ]}
+      <Card>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true
+          }}
+          onFinish={onFinish}
         >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Password!',
-            },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <a className="login-form-forgot" href="/forgot">
-            Forgot password
-          </a>
-        </Form.Item>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!'
+              }
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!'
+              }
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <a className="login-form-forgot" href="/forgot">
+              Forgot password
+            </a>
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" style={{
-   marginRight: "10px"
-    }}>
-            Log in
-          </Button>
-          Or <Link to={urls.signup}>register now!</Link>
-        </Form.Item>
-      </Form>
-    </Card>   
-    </Page>     
-  );
-};
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{
+                marginRight: '10px'
+              }}
+            >
+              Log in
+            </Button>
+            Or <Link to={urls.signup}>register now!</Link>
+          </Form.Item>
+        </Form>
+      </Card>
+    </Page>
+  )
+}
 
 export default LogIn
