60d4bbac3e569d4ed65553420ab0a38af6b8e2d4
SydP <syd.pleno@gmail.com>
2021-03-25 21:59:52 +1100

    fix(backend): watchlist tidy up and documentation

--- backend/RocketMaven/services/WatchlistService.py
+++ backend/RocketMaven/services/WatchlistService.py
@@ -5,29 +5,42 @@ from RocketMaven.commons.pagination import paginate
 
 
 def add_watchlist(investor_id: int, ticker_symbol: str):
-    """ Adds the ticker symbol to the investor's watchlist """
+    """ Adds the ticker symbol to the investor's watchlist 
+        Returns:
+            200 - Asset already in the watchlist
+            201 - Asset added to the watchlist
+            400 - Error adding item to watchlist
+            404 - investor/asset id not found in system
+    """
     try:
-        investor = Investor.query.get_or_404(investor_id)
-        asset = Asset.query.get_or_404(ticker_symbol)
+        investor = Investor.query.get(investor_id)
+        if not investor:
+            return { "msg": "investor id not found in system"}, 404
+        asset = Asset.query.get(ticker_symbol)
+        if not asset:
+            return { "msg": "asset id not found in system"}, 404
         if asset in investor.watchlist_items:
             return { "msg": "asset already in watchlist" }, 200
         investor.watchlist_items.append(asset)
         db.session.commit()
         return { "msg": "asset added to watchlist" }, 201
-    except:
+    except Exception as err:
+        print(err)
         return { "msg": "error adding item to watchlist" }, 400
     
 
 def get_watchlist(investor_id: int):
-    # data = db.session.query(Investor, Asset).\
-    #             filter(
-    #                 investor_watches.asset_id == Asset.ticker_symbol, 
-    #                 investor_watches.investor_id == Investor.id).\
-    #             filter_by(id=investor_id).all()
-    # #data = Investor.query.join(Investor.asset).filter_by(id=investor_id).all()
+    """ Get a paginated list containing the investor's watchlist
+        Returns:
+            200 - paginated list of assets
+            400 - error getting the watchlist
+            404 - investor id not found in the system
+    """
     try:
         schema = AssetSchema(many=True)
-        investor = Investor.query.get_or_404(investor_id)
+        investor = Investor.query.get(investor_id)
+        if not investor:
+            return { "msg": "investor id not found in system"}, 404
         return paginate(investor.watchlist_items, schema)
     except Exception as err:
         print(err)
@@ -35,10 +48,19 @@ def get_watchlist(investor_id: int):
 
 
 def del_watchlist(investor_id: int, ticker_symbol: str):
-    """ Remove the asset from the investor's watchlist """
+    """ Remove the asset from the investor's watchlist 
+        Returns:
+            200 - asset removed from the watchlist
+            400 - asset not in the watchlist or other error deleting the asset from the watchlist
+            404 - investor id not found in the system
+    """
     try:
-        investor = Investor.query.get_or_404(investor_id)
+        investor = Investor.query.get(investor_id)
+        if not investor:
+            return { "msg": "investor id not found in system"}, 404
         asset = Asset.query.get_or_404(ticker_symbol)
+        if not asset:
+            return { "msg": "asset id not found in system"}, 404
         if asset not in investor.watchlist_items:
             return { "msg": "asset not in watchlist"}, 400
         investor.watchlist_items.remove(asset)
