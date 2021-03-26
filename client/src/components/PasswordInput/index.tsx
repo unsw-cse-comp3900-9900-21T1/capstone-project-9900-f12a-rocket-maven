8c809fb6d032e1633867cc7da01b912c9f43111c
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-03-26 13:03:29 +1100

    fix(*): change password validation to suit password generators

--- client/src/components/PasswordInput/index.tsx
+++ client/src/components/PasswordInput/index.tsx
@@ -20,11 +20,8 @@ const PasswordInput = () => {
           validator(_, value) {
             if (!value) return ValidationError('Cannot have an empty password')
 
-            if (value.length < 8)
-              return ValidationError('Password cannot be less than 8 characters')
-
-            if (!/[0-9]/.test(value))
-              return ValidationError('Password must contain at least one digit')
+            if (value.length < 12)
+              return ValidationError('Password cannot be less than 12 characters')
 
             if (!/[A-Z]/.test(value))
               return ValidationError('Password must contain at least one capital letter')
@@ -32,8 +29,13 @@ const PasswordInput = () => {
             if (!/[a-z]/.test(value))
               return ValidationError('Password must contain at least one lowercase letter')
 
+            /*
+            if (!/[0-9]/.test(value))
+              return ValidationError('Password must contain at least one digit')
+
             if (!value.split('').some((ai: string) => special_chars.includes(ai)))
               return ValidationError('Password must contain at least one special character')
+            */
 
             return Promise.resolve()
           }
