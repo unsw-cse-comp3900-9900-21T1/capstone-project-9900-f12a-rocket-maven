fd6f63b068e417e6cc492974a8702045966fc0f0
judemlim <judemlim@gmail.com>
2021-04-06 16:45:13 +1000

    Fixed client useGetPortfolioInfo bugs

--- client/src/pages/Portfolio/PortfolioAssetCreate/index.tsx
+++ client/src/pages/Portfolio/PortfolioAssetCreate/index.tsx
@@ -1,5 +1,4 @@
 import { Fragment } from 'react'
-import { isEmpty } from 'ramda'
 import { Subtitle } from '@rocketmaven/componentsStyled/Typography'
 import { useParams } from 'react-router-dom'
 import { useGetPortfolioInfo } from '@rocketmaven/hooks/http'
@@ -11,20 +10,29 @@ type Params = {
   id: string
 }
 
+type PortfolioFetchInfo = {
+  data: {
+    portfolio: PortfolioInfo,
+  }
+  isLoading: boolean
+}
+
 const PortfolioAssetCreate = () => {
   const { id } = useParams<Params>()
+  const { data, isLoading }: PortfolioFetchInfo = useGetPortfolioInfo(id)
 
-  const portfolioHistory: { portfolio: PortfolioInfo } = useGetPortfolioInfo(id)
-
-  return !isEmpty(portfolioHistory) ? (
-    <Fragment>
-      <Subtitle>Event Create</Subtitle>
-      <PortfolioAssetEditForm portfolioId={id} portfolioInfo={portfolioHistory.portfolio} />
-      {!portfolioHistory.portfolio.competition_portfolio && (
-        <PortfolioAssetCSVUpload portfolioId={id} />
-      )}
-    </Fragment>
-  ) : null
+  return (
+    isLoading
+    ? null
+    :
+      <Fragment>
+        <Subtitle>Event Create</Subtitle>
+        <PortfolioAssetEditForm portfolioId={id} portfolioInfo={data.portfolio} />
+        {!data.portfolio.competition_portfolio && (
+          <PortfolioAssetCSVUpload portfolioId={id} />
+        )}
+      </Fragment>
+  )
 }
 
 export default PortfolioAssetCreate
