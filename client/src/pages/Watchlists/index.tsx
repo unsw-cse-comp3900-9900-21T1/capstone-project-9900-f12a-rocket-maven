8f2e2f4ea099638d5ba46f17f53dc704f6588f79
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-04-06 16:31:41 +1000

    feat(frontend): add additional asset info in watchlist

--- client/src/pages/Watchlists/index.tsx
+++ client/src/pages/Watchlists/index.tsx
@@ -1,4 +1,5 @@
 import Page from '@rocketmaven/pages/_Page'
+import { Link } from 'react-router-dom'
 import { urls } from '@rocketmaven/data/urls'
 import { Table, Form, Button } from 'antd'
 import React, { useEffect, useState } from 'react'
@@ -70,19 +71,83 @@ const Watchlists = () => {
     const columns = [
       {
         title: 'Ticker',
-        dataIndex: 'ticker_symbol'
+        dataIndex: 'ticker_symbol',
+        render: (value: string) => (
+          <Link
+            to={`/asset/${value}`}
+            style={{
+              marginRight: '8px',
+              marginBottom: '12px'
+            }}
+          >
+            {value}
+          </Link>
+        )
       },
       {
         title: 'Name',
-        dataIndex: 'name'
+        dataIndex: 'asset_additional',
+        render: (value: string) => {
+          console.log(JSON.parse(value))
+          const asset_additional = JSON.parse(value)
+          const focus = asset_additional.longName
+          if (focus) {
+            return <div>{focus}</div>
+          }
+          return null
+        }
       },
       {
         title: 'Price',
         dataIndex: 'current_price'
       },
       {
+        title: 'Change',
+        dataIndex: 'asset_additional',
+        render: (value: string) => {
+          const asset_additional = JSON.parse(value)
+          const focus = asset_additional.regularMarketChange
+          if (focus) {
+            return <div>{focus.fmt}</div>
+          }
+          return null
+        }
+      },
+      {
         title: 'Market Cap',
-        dataIndex: 'market_cap'
+        dataIndex: 'asset_additional',
+        render: (value: string) => {
+          const asset_additional = JSON.parse(value)
+          const focus = asset_additional.marketCap
+          if (focus) {
+            return <div>{focus.fmt}</div>
+          }
+          return null
+        }
+      },
+      {
+        title: '52-Week High',
+        dataIndex: 'asset_additional',
+        render: (value: string) => {
+          const asset_additional = JSON.parse(value)
+          const focus = asset_additional.fiftyTwoWeekHigh
+          if (focus) {
+            return <div>{focus.fmt}</div>
+          }
+          return null
+        }
+      },
+      {
+        title: '52-Week Low',
+        dataIndex: 'asset_additional',
+        render: (value: string) => {
+          const asset_additional = JSON.parse(value)
+          const focus = asset_additional.fiftyTwoWeekLow
+          if (focus) {
+            return <div>{focus.fmt}</div>
+          }
+          return null
+        }
       },
       {
         title: 'Delete',
