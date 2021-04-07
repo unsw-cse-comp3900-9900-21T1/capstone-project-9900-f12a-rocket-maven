9988c8ea6b51a9da57e4a4c4ba354287b26b79c3
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-04-07 12:50:41 +1000

    fix(backend): solve deleted competition portfolios showing

--- backend/RocketMaven/services/CompetitionService.py
+++ backend/RocketMaven/services/CompetitionService.py
@@ -2,11 +2,12 @@ from RocketMaven.api.schemas import LeaderboardSchema
 from RocketMaven.models import Portfolio
 from RocketMaven.commons.pagination import paginate
 
+
 def get_leaderboard():
 
     try:
         schema = LeaderboardSchema(many=True)
-        query = Portfolio.query.filter_by(competition_portfolio=True)
+        query = Portfolio.query.filter_by(competition_portfolio=True, deleted=False)
         return paginate(query, schema)
 
     except Exception as e:
