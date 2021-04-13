26f3152162743588d325476a5889fdcbfbc74ca9
SydP <syd.pleno@gmail.com>
2021-04-13 22:53:49 +1000

    chore(backend): unittest for asset endpoints

--- backend/tests/test_assets.py
+++ backend/tests/test_assets.py
@@ -5,56 +5,62 @@ from flask import url_for
 from RocketMaven.models import Asset
 
 
-@pytest.fixture
-def asset(db):
-    asset = Asset(
-        ticker_symbol="VIRT:A",
-        name="Virtual Holding A",
-        industry="Virtual",
-        current_price=100,
-        data_source="VIRTUAL",
-        country="AU",
-        currency="AUD",
-    )
+def test_search_assets(client, asset):
+    # test 400
+    url = url_for("api.asset_search")
+    resp = client.get(url) 
+    assert resp.status_code == 400
 
-    db.session.add(asset)
-    return asset
+    # test 200
+    query = {
+        "q": "B"
+    }
+    resp = client.get(url, query_string=query) 
+    assert resp.status_code == 200
+    assert any(res["ticker_symbol"] == asset.ticker_symbol for res in resp.get_json()["results"])
     
 
-def test_search_assets():
-    # test 400
-
-    # test 404
-
-    # test 200
-    pass
-
-
-def test_search_assets_portfolio():
+def test_search_assets_portfolio(client, portfolio, asset, normal_headers):
     # test 401
+    url = url_for("api.user_asset_search", portfolio_id=portfolio.id)
+    resp = client.get(url)
+    assert resp.status_code == 401
 
     # test 400
-
-    # test 404
-
-    # test 200
-    pass
-
-
-def test_get_asset():
-    # test 400
-
-    # test 404
+    url = url_for("api.user_asset_search", portfolio_id=portfolio.id)
+    resp = client.get(url, headers=normal_headers)
+    assert resp.status_code == 400
 
     # test 200
-    pass
+    url = url_for("api.user_asset_search", portfolio_id=portfolio.id)
+    resp = client.get(url, query_string={"q":"B"}, headers=normal_headers)
+    print(resp.get_json())
+    assert resp.status_code == 200
 
-def test_get_asset_price():
-    # test 400
+
+def test_get_asset(client, asset):
+    # test 400 - cannot test due to endpoint routing requiring this value
+    
+    # test 404
+    url = url_for("api.asset_by_ticker", ticker_symbol="unknown")
+    resp = client.get(url)
+    assert resp.status_code == 404
+    
+    # test 200
+    url = url_for("api.asset_by_ticker", ticker_symbol=asset.ticker_symbol)
+    resp = client.get(url)
+    assert resp.status_code == 200
+    
+    
+def test_get_asset_price(client, asset):
+    # test 400 - cannot test due to endpoint routing requiring this value
 
     # test 404
-
+    url = url_for("api.asset_price_by_ticker", ticker_symbol="unknown")
+    resp = client.get(url)
+    assert resp.status_code == 404
+    
     # test 200
-    pass
-
-
+    url = url_for("api.asset_price_by_ticker", ticker_symbol=asset.ticker_symbol)
+    resp = client.get(url)
+    assert resp.status_code == 200
