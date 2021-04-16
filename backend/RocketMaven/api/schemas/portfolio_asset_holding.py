6073f1da4c10fd9ed339e281220c6df8a99f13d2
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-04-16 11:53:50 +1000

    fix(backend): better recommendation algorithm

--- backend/RocketMaven/api/schemas/portfolio_asset_holding.py
+++ backend/RocketMaven/api/schemas/portfolio_asset_holding.py
@@ -1,18 +1,25 @@
 from RocketMaven.models import PortfolioAssetHolding
+from RocketMaven.api.schemas import AssetSchema
 from RocketMaven.extensions import ma, db
 
 
 class PortfolioAssetHoldingSchema(ma.SQLAlchemyAutoSchema):
     """  A schema for a asset holding in a portfolio """
 
+    asset = ma.Nested(AssetSchema, many=False)
+
     class Meta:
         model = PortfolioAssetHolding
         sqla_session = db.session
         load_instance = True
 
-    _unrealised_units = ma.auto_field(data_key="unrealised_units", attribute="unrealised_units")
+    _unrealised_units = ma.auto_field(
+        data_key="unrealised_units", attribute="unrealised_units"
+    )
     _current_value = ma.auto_field(data_key="current_value", attribute="current_value")
-    _purchase_value = ma.auto_field(data_key="purchase_value", attribute="purchase_value")
+    _purchase_value = ma.auto_field(
+        data_key="purchase_value", attribute="purchase_value"
+    )
 
     asset_id = ma.auto_field()
     portfolio_id = ma.auto_field()
