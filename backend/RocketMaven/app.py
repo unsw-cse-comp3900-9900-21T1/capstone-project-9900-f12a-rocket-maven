077a52523796f201bab3b71a257b924bc6df8443
Andy Tran <22376474+trana8525@users.noreply.github.com>
2021-04-16 17:20:43 +1000

    chore(*): render front end to back end

--- backend/RocketMaven/app.py
+++ backend/RocketMaven/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from RocketMaven import api
 from RocketMaven import auth
 from RocketMaven.extensions import apispec
@@ -19,35 +19,54 @@ if sys.platform.lower() == "win32":
 
 def create_app(testing=False):
     """Application factory, used to create application"""
-    app = Flask("RocketMaven")
+
+    app = Flask("RocketMaven", static_folder=None)
     app.config.from_object("RocketMaven.config")
 
     if testing is True:
         app.config["TESTING"] = True
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=60*24*30)
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=60 * 24 * 30)
 
     configure_extensions(app)
     configure_apispec(app)
     register_blueprints(app)
 
-    @app.route("/", methods=["GET"])
-    def custom_index():
-        return "<a href='swagger-ui'>Swagger UI</a>", 200
+    # http://stackoverflow.com/questions/30620276/flask-and-react-routing/50660437#50660437
+    @app.route("/", defaults={"path": ""})
+    @app.route("/<path:path>")
+    def react_static(path):
+        print(path, os.path.join(app.root_path + "/web/", path))
+        if path != "" and os.path.exists(os.path.join(app.root_path + "/web/", path)):
+            return send_from_directory(app.root_path + "/web/", path)
+        else:
+            return send_from_directory(app.root_path + "/web/", "index.html")
+
+    # https://stackoverflow.com/questions/9513072/more-than-one-static-path-in-local-flask-instance
+    @app.route("/swagger/<path:filename>")
+    def swagger_static(filename):
+        return send_from_directory(app.root_path + "/swagger/", filename)
+
+    # def custom_index():
+    #     return "<a href='swagger-ui'>Swagger UI</a>", 200
 
     @app.route("/pw_reset", methods=["GET", "POST"])
     def pw_reset():
         key = request.args.get("key")
         print(key)
-        return """
+        return (
+            """
             <form action="/api/v1/pw_reset" method="post">
               <label>Enter new password:</label>
               <input type="text" id="password" name="password"><br><br>
               <label>Repeat your password:</label>
               <input type="text" id="confirmation" name="confirmation"><br><br>
               <input type="submit" value="Submit">
-              <input type="hidden" id="evc" name="evc" value="""+key+""">
+              <input type="hidden" id="evc" name="evc" value="""
+            + key
+            + """>
             </form>
         """
+        )
 
     return app
 
